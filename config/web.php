078344a17ba7e98fedcfb5ef01cbb850526f0bd9
semivori <semivoriii@gmail.com>
2022-04-18 00:38:43 +0300

    t

--- config/web.php
+++ config/web.php
@@ -2,7 +2,7 @@
 
 $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
-$dbLocal = require __DIR__ . '/db.php';
+$dbLocal = require __DIR__ . '/db.local.php';
 $db = array_merge($db, $dbLocal);
 
 $config = [
